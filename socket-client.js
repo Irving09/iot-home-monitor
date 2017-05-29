a225c0aff6ceee5180448791849fabfe4be51860
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-29 16:13:12 -0700

    Stream finally working

--- socket-client.js
+++ socket-client.js
@@ -10,13 +10,12 @@
     console.log('UI connected to socket server');
   });
 
-  socket.on(UI_TOPIC, function(data) {
-    // data should be hello world here
-    console.log(data);
-  });
-
-  socket.on('disconnect', function() {
-
+  socket.on(UI_TOPIC, function(imageStream) {
+    console.log('=================');
+    console.log('received image stream');
+    let img = global.document.getElementById('image-stream');
+    img.setAttribute('src', 'data:image/jpeg;base64,' + imageStream);
+    console.log('=================');
   });
 
 })(window);
