7edeacff01c37d7274eab44b97138af0b6cff378
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-29 16:51:06 -0700

    Removed console.log bar lines

--- socket-client.js
+++ socket-client.js
@@ -13,11 +13,9 @@
   });
 
   socket.on(UI_TOPIC, function(imageStream) {
-    console.log('=================');
     console.log('received image stream');
     let img = global.document.getElementById('image-stream');
     img.setAttribute('src', 'data:image/jpeg;base64,' + imageStream);
-    console.log('=================');
   });
 
 })(window);
