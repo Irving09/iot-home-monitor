c9dc2b7cff63817dc448b1cd9932e3bb4781c287
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-29 19:51:18 -0700

    Using public ip instead of private ip

--- socket-client.js
+++ socket-client.js
@@ -2,16 +2,15 @@
   const UI_TOPIC = 'iot-home-intruder-ui';
 
   // TODO change this to EC2 Socket server
-  const SOCKET_PRIVATE_IP = 'http://172.31.30.51:3000';
+  const SOCKET_PUBLIC_IP = 'http://ec2-52-34-34-51.us-west-2.compute.amazonaws.com:3000';
 
-  const socket = global.io(SOCKET_PRIVATE_IP, {
+  const socket = global.io(SOCKET_PUBLIC_IP, {
     reconnection: true
   });
 
   socket.on('connect', function() {
-    console.log('socketPrivateIp connected to socket server');
+    console.log('socketPublicIp connected to socket server');
   });
-
 
   socket.on(UI_TOPIC, function(imageStream) {
     console.log('received image stream');
