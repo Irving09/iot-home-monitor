111de0311901a8dbd0e432173f1436c4ddcc9e67
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-29 19:32:11 -0700

    Test

--- socket-client.js
+++ socket-client.js
@@ -2,15 +2,27 @@
   const UI_TOPIC = 'iot-home-intruder-ui';
 
   // TODO change this to EC2 Socket server
-  const SOCKET_SERVER = 'http://localhost:3000';
+  const SOCKET_LOCAL = 'http://localhost:3000';
+  const SOCKET_PRIVATE_IP = 'http://172.31.30.51:3000';
 
-  const socket = global.io(SOCKET_SERVER, {
+  const socket = global.io(SOCKET_LOCAL, {
     reconnection: true
   });
 
   socket.on('connect', function() {
-    console.log('UI connected to socket server');
+    console.log('socketLocal connected to socket server');
   });
+
+
+  const socket2 = global.io(SOCKET_PRIVATE_IP, {
+    reconnection: true
+  });
+
+  socket2.on('connect', function() {
+    console.log('socketPrivateIp connected to socket server');
+  });
+
+
 
   socket.on(UI_TOPIC, function(imageStream) {
     console.log('received image stream');
